70fa1428d2245787984303030d9e57d115d78742
denis.makarov <chaspik16@gmail.com>
2023-05-31 19:25:27 +0300

    2.13. Шаблонизируй это - 1.2

--- src/main.js
+++ src/main.js
@@ -1,35 +1,20 @@
-/*import NewTaskButtonView from './view/new-task-button-view.js';*/
 import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
-<<<<<<< HEAD
-import TripListPresenter from './presenter/trip-list-presenter.js';
-import TripPointEditPresenter from './presenter/trip-point-edit-presenter.js';
-=======
 import TripPresenter from './presenter/trip-presenter.js';
->>>>>>> aa7aec6 (2.13. Шаблонизируй это - 1.1)
 import {render, RenderPosition} from './render.js';
+import TripsModel from './model/model.js';
 
 const tripHeaderElement = document.querySelector('.trip-main');
 const tripHeaderFilterElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
-const tripListPresenter = new TripListPresenter({tripListContainer: tripEventsElement});
 
-<<<<<<< HEAD
-const tripPointEditPresenter = new TripPointEditPresenter({tripPointEditContainer: tripEventsElement});
-=======
 
 const tripsModel = new TripsModel();
 
 const tripPresenter = new TripPresenter({tripPointEditContainer: tripEventsElement, tripsModel});
->>>>>>> aa7aec6 (2.13. Шаблонизируй это - 1.1)
 
 render(new TripInfoView(), tripHeaderElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), tripHeaderFilterElement);
 
-<<<<<<< HEAD
-tripListPresenter.init();
-/*tripPointEditPresenter.init();*/
-=======
 
 tripPresenter.init();
->>>>>>> aa7aec6 (2.13. Шаблонизируй это - 1.1)
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,6 +1,4 @@
 import {createElement} from '../render.js';
-<<<<<<< HEAD
-=======
 import {humanizeTripDueDate, getPointDuration} from '../utils.js';
 import {DATE_FORMAT} from '../const.js';
 
@@ -24,37 +22,29 @@ function createPointTemplate(trip, pointOffers, pointDestinations) {
   const favoriteClassName = isFavorite
     ? 'event__favorite-btn event__favorite-btn--active'
     : 'event__favorite-btn';
->>>>>>> aa7aec6 (2.13. Шаблонизируй это - 1.1)
 
-function createPointTemplate() {
   return (`
     <li class="trip-events__item">
       <div class="event">
-        <time class="event__date" datetime="2019-03-18">MAR 18</time>
+        <time class="event__date" datetime="${humanizeTripDueDate(dateFrom, DATE_FORMAT.YEAR_MONTH_DAY)}">${humanizeTripDueDate(dateFrom, DATE_FORMAT.MONTH_DAY)}</time>
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Taxi Amsterdam</h3>
+        <h3 class="event__title">${type} ${tripCity}</h3>
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            <time class="event__start-time" datetime="${humanizeTripDueDate(dateFrom, DATE_FORMAT.YEAR_MONTH_DAY_TIME)}">${humanizeTripDueDate(dateFrom, DATE_FORMAT.HOUR_MINUTES)}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+            <time class="event__end-time" datetime="${humanizeTripDueDate(dateTo, DATE_FORMAT.YEAR_MONTH_DAY_TIME)}">${humanizeTripDueDate(dateTo, DATE_FORMAT.HOUR_MINUTES)}</time>
           </p>
-          <p class="event__duration">30M</p>
+          <p class="event__duration">${getPointDuration(dateFrom, dateTo)}</p>
         </div>
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
-        <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">20</span>
-          </li>
-        </ul>
-        <button class="event__favorite-btn event__favorite-btn--active" type="button">
+        ${createOffersTemplate(pointOffer)}
+        <button class="${favoriteClassName}" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -69,8 +59,16 @@ function createPointTemplate() {
 }
 
 export default class PointView {
+
+  constructor({trip, offers, destinations}) {
+
+    this.trip = trip;
+    this.pointOffers = offers;
+    this.pointDestinations = destinations;
+  }
+
   getTemplate() {
-    return createPointTemplate();
+    return createPointTemplate(this.trip, this.pointOffers, this.pointDestinations);
   }
 
   getElement() {
@@ -85,4 +83,3 @@ export default class PointView {
     this.element = null;
   }
 }
-
